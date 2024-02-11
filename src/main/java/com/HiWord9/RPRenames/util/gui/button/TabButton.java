d29406d1c7e74a0e9fe0c681509efc711db3b818
mirik <miriklevshenkov@gmail.com>
2024-02-10 16:31:53 -0800

    tooltips for tabs

--- src/main/java/com/HiWord9/RPRenames/util/gui/button/TabButton.java
+++ src/main/java/com/HiWord9/RPRenames/util/gui/button/TabButton.java
@@ -3,17 +3,23 @@ package com.HiWord9.RPRenames.util.gui.button;
 import com.HiWord9.RPRenames.AnvilScreenMixinAccessor;
 import com.HiWord9.RPRenames.RPRenames;
 import com.HiWord9.RPRenames.util.Tabs;
+import com.HiWord9.RPRenames.util.gui.Graphics;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.ingame.AnvilScreen;
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
+import net.minecraft.client.gui.tooltip.HoveredTooltipPositioner;
+import net.minecraft.client.gui.tooltip.TooltipComponent;
 import net.minecraft.client.gui.widget.ClickableWidget;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 import java.util.Arrays;
+import java.util.List;
 
 public class TabButton extends ClickableWidget {
     private static final Identifier TEXTURE = new Identifier(RPRenames.MOD_ID, "textures/gui/tabs.png");
+    private static final String TRANSLATION_PREFIX = "rprenames.gui.tabs.tooltip.";
 
     public static final int buttonWidth = 33;
     public static final int buttonHeight = 26;
@@ -39,6 +45,16 @@ public class TabButton extends ClickableWidget {
             int u = currentTab == tab ? selectedOffsetU : 0;
             int v = index * typeOffsetV;
             context.drawTexture(TEXTURE, getX(), getY(), u, v, getWidth(), getHeight(), textureWidth, textureHeight);
+
+            if (isMouseOver(mouseX, mouseY)) {
+                Graphics.drawTooltip(
+                        context,
+                        MinecraftClient.getInstance().textRenderer,
+                        List.of(TooltipComponent.of(Text.translatable(TRANSLATION_PREFIX + tab.toString()).asOrderedText())),
+                        mouseX, mouseY,
+                        HoveredTooltipPositioner.INSTANCE
+                );
+            }
         }
     }
 
@@ -61,7 +77,5 @@ public class TabButton extends ClickableWidget {
     }
 
     @Override
-    protected void appendClickableNarrations(NarrationMessageBuilder builder) {
-
-    }
+    protected void appendClickableNarrations(NarrationMessageBuilder builder) {}
 }
