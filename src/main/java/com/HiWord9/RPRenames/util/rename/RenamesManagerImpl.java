effdc742bb79394c845320a1537b4fa256a49249
mirik <miriklevshenkov@gmail.com>
2025-09-20 03:51:40 +0200

    code cleanup RenamesManagerImpl

--- src/main/java/com/HiWord9/RPRenames/util/rename/RenamesManagerImpl.java
+++ src/main/java/com/HiWord9/RPRenames/util/rename/RenamesManagerImpl.java
@@ -3,10 +3,7 @@ package com.HiWord9.RPRenames.util.rename;
 import com.HiWord9.RPRenames.util.rename.type.Rename;
 import net.minecraft.item.Item;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class RenamesManagerImpl<R extends Rename> implements RenamesManager<R> {
@@ -14,18 +11,15 @@ public class RenamesManagerImpl<R extends Rename> implements RenamesManager<R> {
 
     public List<R> getAllRenames() {
         return renames
-                .entrySet().stream()
-                .flatMap(entry -> entry.getValue().stream())
+                .values().stream()
+                .flatMap(Collection::stream)
                 .distinct()
                 .collect(Collectors.toList());
     }
 
     public List<R> getRenames(Item item) {
-        if (renames.containsKey(item)) {
-            return List.copyOf(renames.get(item));
-        } else {
-            return List.of();
-        }
+        var l = renames.get(item);
+        return l == null ? List.of() : List.copyOf(l);
     }
 
     public void addRename(Item item, R rename) {
@@ -33,9 +27,8 @@ public class RenamesManagerImpl<R extends Rename> implements RenamesManager<R> {
     }
 
     public void removeRename(Item item, R rename) {
-        if (renames.containsKey(item)) {
-            renames.get(item).remove(rename);
-        }
+        var l = renames.get(item);
+        if (l != null) l.remove(rename);
     }
 
     public void clearRenames() {
