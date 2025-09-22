f752533c035ba6c9babe2d96fbb87fb52eb01ea2
mirik <miriklevshenkov@gmail.com>
2025-09-23 01:37:02 +0200

    GhostCraft.GhostSlot extends ClickableWidget

--- src/main/java/com/HiWord9/RPRenames/mod/gui/widget/GhostCraft.java
+++ src/main/java/com/HiWord9/RPRenames/mod/gui/widget/GhostCraft.java
@@ -5,13 +5,15 @@ import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.Drawable;
 import net.minecraft.client.gui.Element;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder;
+import net.minecraft.client.gui.widget.ClickableWidget;
 import net.minecraft.item.ItemStack;
 
 import static com.HiWord9.RPRenames.mod.gui.Graphics.*;
 import static com.HiWord9.RPRenames.mod.util.Util.*;
 
 public class GhostCraft implements Drawable, Element {
-    GhostSlot[] slots;
+    public final GhostSlot[] slots;
     public final int length;
 
     private boolean doRender = false;
@@ -54,7 +56,7 @@ public class GhostCraft implements Drawable, Element {
     public void setFocused(boolean focused) {}
 
     @Override
-    public boolean isFocused() {return false;}
+    public boolean isFocused() { return false; }
 
     public void setRender(boolean doRender) {
         this.doRender = doRender;
@@ -88,41 +90,19 @@ public class GhostCraft implements Drawable, Element {
     }
 
     public void offsetX(int x) {
-        for (GhostSlot slot : slots) slot.x += x;
+        for (GhostSlot slot : slots) slot.setX(slot.getX() + x);
     }
 
-    public static class GhostSlot implements Drawable {
-        int x;
-        int y;
-        ItemStack content;
-        boolean forceHighlight = false;
+    public static class GhostSlot extends ClickableWidget {
+        protected ItemStack content;
+        protected boolean forceHighlight = false;
 
         public GhostSlot(int x, int y) {
-            this.x = x;
-            this.y = y;
+            this(x, y, SLOT_SIZE);
         }
 
-        @Override
-        public void render(DrawContext context, int mouseX, int mouseY, float delta) {
-            if (content != null && !content.isEmpty()) {
-                Graphics.renderStack(context, content, x + 1, y + 1);
-                if (isMouseOver(mouseX, mouseY)) {
-                    context.drawTooltip(textRenderer(), Screen.getTooltipFromItem(client(), content), mouseX, mouseY);
-                }
-            }
-            int color;
-            if (forceHighlight) {
-                color = HIGHLIGHT_COLOR_SECOND;
-            } else if (content != null) {
-                color = HIGHLIGHT_COLOR_WRONG;
-            } else {
-                return;
-            }
-            context.fill(x, y, x + SLOT_SIZE, y + SLOT_SIZE, color);
-        }
-
-        public boolean isMouseOver(double mouseX, double mouseY) {
-            return mouseX > x && mouseX < x + SLOT_SIZE && mouseY > y && mouseY < y + SLOT_SIZE;
+        protected GhostSlot(int x, int y, int size) {
+            super(x, y, size, size, null);
         }
 
         public void setForceHighlight(boolean forceHighlight) {
@@ -132,5 +112,38 @@ public class GhostCraft implements Drawable, Element {
         public void setContent(ItemStack content) {
             this.content = content;
         }
+
+        @Override
+        public void renderWidget(DrawContext context, int mouseX, int mouseY, float delta) {
+            if (content != null && !content.isEmpty()) {
+                Graphics.renderStack(context, content, getX() + 1, getY() + 1);
+                if (isMouseOver(mouseX, mouseY)) {
+                    context.drawTooltip(textRenderer(), Screen.getTooltipFromItem(client(), content), mouseX, mouseY);
+                }
+            }
+            int color;
+
+            if (forceHighlight) color = getForceHighlightColor();
+            else if (content != null) color = getWrongHighlightColor();
+            else return;
+
+            context.fill(getX(), getY(), getX() + getWidth(), getY() + getHeight(), color);
+        }
+
+        @Override
+        public boolean mouseClicked(double mouseX, double mouseY, int button) {
+            return active && visible && isMouseOver(mouseX, mouseY);
+        }
+
+        protected int getForceHighlightColor() {
+            return HIGHLIGHT_COLOR_SECOND;
+        }
+
+        protected int getWrongHighlightColor() {
+            return HIGHLIGHT_COLOR_WRONG;
+        }
+
+        @Override
+        protected void appendClickableNarrations(NarrationMessageBuilder builder) {}
     }
 }
