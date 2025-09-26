a6b8382c21949fed48ce1b9108249f5d8b3923e2
mirik <miriklevshenkov@gmail.com>
2025-09-26 16:13:03 +0200

    added Condition#apply
    and implemented it for ComponentSelectProperty

--- src/main/java/com/HiWord9/RPRenames/mod/impl/renames_manager/updatable/parser/item_model/condition/Condition.java
+++ src/main/java/com/HiWord9/RPRenames/mod/impl/renames_manager/updatable/parser/item_model/condition/Condition.java
@@ -1,8 +1,12 @@
 package com.HiWord9.RPRenames.mod.impl.renames_manager.updatable.parser.item_model.condition;
 
+import net.minecraft.item.ItemStack;
+
 public interface Condition {
     FallbackCondition FALLBACK = new FallbackCondition();
     CompositeCondition COMPOSITE = new CompositeCondition();
+
+    default void apply(ItemStack stack) {}
 
     class FallbackCondition implements Condition {
         private FallbackCondition() {}
--- src/main/java/com/HiWord9/RPRenames/mod/impl/renames_manager/updatable/parser/item_model/condition/SelectCondition.java
+++ src/main/java/com/HiWord9/RPRenames/mod/impl/renames_manager/updatable/parser/item_model/condition/SelectCondition.java
@@ -1,10 +1,21 @@
 package com.HiWord9.RPRenames.mod.impl.renames_manager.updatable.parser.item_model.condition;
 
+import net.minecraft.client.render.item.property.select.ComponentSelectProperty;
 import net.minecraft.client.render.item.property.select.SelectProperty;
+import net.minecraft.item.ItemStack;
 
 import java.util.List;
 
 public record SelectCondition<P extends SelectProperty<V>, V>(
         P property,
         List<V> values
-) implements Condition {}
+) implements Condition {
+    @Override
+    @SuppressWarnings("unchecked") // todo wrap with try
+    public void apply(ItemStack stack) {
+        if (property instanceof ComponentSelectProperty<?>) {
+            var componentSelectProperty = (ComponentSelectProperty<V>) property;
+            stack.set(componentSelectProperty.componentType(), values.getFirst());
+        } // todo fill for other cases
+    }
+}
